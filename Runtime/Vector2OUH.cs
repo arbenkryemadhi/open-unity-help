52684077ab4ce6b16ba76621c4cac7583a5cef33
arbenkryemadhi <arbenofc@gmail.com>
2022-08-16 16:55:58 +0200

    ColorOUH, RectOUH, TransformOUH

--- Runtime/Vector2OUH.cs
+++ Runtime/Vector2OUH.cs
@@ -13,17 +13,29 @@ namespace OpenUnityHelp
 		///<summary>
 		/// Returns a pixel perfect Vector2 by changing its coordinates to ints.
 		///</summary>
-		public static Vector2 GetPixelPerfect(Vector2 position)
+		public static Vector2 GetPixelPerfect(Vector2 vector2)
 		{
-			return new Vector2((int)position.x, (int)position.y);
+			return new Vector2((int)vector2.x, (int)vector2.y);
 		}
+
 
 		///<summary>
 		/// Returns a rotated Vector2 by an angle.
 		///</summary>
-		public static Vector2 GetRotatedVector(Vector2 vector, float angle)
+		public static Vector2 GetRotatedVector(Vector2 vector2, float angle)
 		{
-			return Quaternion.AngleAxis(angle, Vector3.forward) * vector;
+			return Quaternion.AngleAxis(angle, Vector3.forward) * vector2;
+		}
+
+
+		///<summary>
+		/// Returns the angle of the Vector2.
+		///</summary>
+		public static float GetVectorAngle(Vector2 vector2)
+		{
+			return vector2.y > 0
+					   ? Vector2.Angle(new Vector2(1, 0), vector2)
+					   : -Vector2.Angle(new Vector2(1, 0), vector2);
 		}
 	}
 }
